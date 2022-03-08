8526fd1fb0e03b94f2b21ca75fcbb13868daa2dc
A_ye <1464102511@qq.com>
2022-03-08 10:16:28 +0800

    项目初始化-引入Vant组件库

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,20 @@
 <template>
   <div id="app">
-   <!-- 路由出口 -->
-    <router-view/>
+    <!-- 路由出口 -->
+    <router-view />
     <h1>阳光头条</h1>
+    <div>
+      <i class="toutiao toutiao-dianzan"></i>
+    </div>
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
+    <van-cell-group inset>
+      <van-cell title="单元格" value="内容" />
+      <van-cell title="单元格" value="内容" label="描述信息" />
+    </van-cell-group>
   </div>
 </template>
 
@@ -13,5 +25,4 @@ export default {
 </script>
 
 <style lang="less">
-
 </style>
